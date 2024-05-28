4ac348f96b9a1ef4a75c10e91aeea5de94c5a7e5
SMangera8 <111564099+SMangera8@users.noreply.github.com>
2024-05-28 13:14:01 +0200

    Added Signup Page

---
+++ frontend/pages/Signup/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 class="text-4xl font-bold text-pink">Signup Page</h1>
+      <NuxtLink to="/" class="text-2xl font-bold text-black hover:underline">Go to Home</NuxtLink>
+    </div>
+</template>
