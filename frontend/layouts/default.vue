144c374ff96a8f612a1cd31b9d115ed6b6ab6cea
Xavier893 <xavierrynolds@gmail.com>
2024-05-29 19:16:49 +0200

    Added default layout for navbar

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <Navbar />
+    </header>
+  </div>
+
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue"
+</script>
