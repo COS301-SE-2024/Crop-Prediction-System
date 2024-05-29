c8263b66a39a585f59e4880366451ca4b5054128
Xavier893 <xavierrynolds@gmail.com>
2024-05-29 19:18:19 +0200

    Added routing pages

---
+++ frontend/pages/inputs/manage-fields/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <p>Manage Fields Page</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ frontend/pages/model-training/manage-model/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <p>Manage Model Page</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
