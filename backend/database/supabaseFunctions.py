ec731ae2283d384de5566936bc663393e3454611
Iwan de Jong <idj.idejong@gmail.com>
2024-05-30 21:41:27 +0200

    made sure env variables work

--- backend/database/supabaseFunctions.py
+++ backend/database/supabaseFunctions.py
@@ -1,10 +1,13 @@
 import os
 from supabase import create_client, Client
-import json
+from dotenv import load_dotenv
 
-url: str = "https://iimtpbzfrdcuuklwnprq.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpbXRwYnpmcmRjdXVrbHducHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4MTQ1MDMsImV4cCI6MjAzMTM5MDUwM30.o2gbIkgZaTQlFRLabs-abzkim462xatVumMJXo06m6w"
-supabase: Client = create_client(url, key)
+load_dotenv()
+
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+
+sbClient: Client = create_client(url, key)
 
 def sbGetFieldInfo():
-    return supabase.table("fieldInfo").select("*").execute().data
+    return sbClient.table("fieldInfo").select("*").execute().data
