4d2c414958cbe1379e3915baabf21a551033ef3d
Xavier893 <xavierrynolds@gmail.com>
2024-05-31 23:58:53 +0200

    Fixed API key leak for Google Maps

--- frontend/plugins/maps.client.js
+++ frontend/plugins/maps.client.js
@@ -1,10 +1,10 @@
 import { Loader } from '@googlemaps/js-api-loader'
 
 export default defineNuxtPlugin((nuxtApp) => {
-	const loader = new Loader({
-		apiKey: 'AIzaSyAM5zZT_BdKRMrQBSxfIO7XeygeSFAmJOo',
-		libraries: ['places', 'drawing'], // Add any needed libraries
-	})
+  const loader = new Loader({
+    apiKey: process.env.GOOGLE_MAPS_API_KEY,
+    libraries: ['places', 'drawing'], // Add any needed libraries
+  })
 
-	nuxtApp.provide('mapsLoader', loader)
+  nuxtApp.provide('mapsLoader', loader)
 })
