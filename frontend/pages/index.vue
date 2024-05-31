ee7b1265203026f33e6f4424b415b109c46ef7a0
Xavier893 <xavierrynolds@gmail.com>
2024-05-31 23:42:49 +0200

    Added Maps to /inputs/manage-field route and basic Field Save flow (not done)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen">
-    <h1 class="text-4xl font-light text-black">This is working, Tailwind CSS!</h1>
-    <NuxtLink to="/about" class="text-lg text-black hover:underline">Go to About page</NuxtLink>
-  </div>
+	<div class="flex flex-col items-center justify-center min-h-screen">
+		<h1 class="text-4xl font-light text-black">This is working, Tailwind CSS is working!</h1>
+	</div>
 </template>
