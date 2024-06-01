941b0f16b78671fa769103a2ce28e58f55fee6cc
Xavier893 <xavierrynolds@gmail.com>
2024-06-02 00:11:42 +0200

    Added logout button to log users out

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -5,8 +5,14 @@
 			<img src="../assets/logo.png" alt="Logo" class="h-full object-cover self-center" />
 			<div class="flex flex-row items-center gap-4">
 				<i class="pi pi-bell" style="font-size: 1.5rem" />
-				<i class="pi pi-user" style="font-size: 1.5rem" />
+				<i class="pi pi-user" @click="toggle" style="font-size: 1.5rem" />
 				<i class="pi pi-cog" style="font-size: 1.5rem" />
+				<OverlayPanel ref="op" class="w-56 border border-surface-border">
+					<div class="flex flex-col items-center gap-5">
+						<h3>{{ user.email }}</h3>
+						<Button label="Log Out" severity="danger" class="w-full" @click="signOut" />
+					</div>
+				</OverlayPanel>
 			</div>
 		</div>
 	</nav>
@@ -14,4 +20,28 @@
 
 <script setup lang="ts">
 import Sidebar from '../components/Sidebar.vue'
+import { ref } from 'vue'
+import Button from 'primevue/button'
+
+const user = useSupabaseUser()
+const client = useSupabaseClient()
+
+const op = ref()
+
+const toggle = (event) => {
+	op.value.toggle(event)
+}
+
+const signOut = async () => {
+	try {
+		const { error } = await client.auth.signOut()
+		if (!error) {
+			navigateTo('/login')
+		} else {
+			throw error
+		}
+	} catch (error) {
+		console.log(error)
+	}
+}
 </script>
