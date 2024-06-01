c9ec05c2285082dd77a21cfa286f2d6c6ab72c0e
Xavier893 <xavierrynolds@gmail.com>
2024-06-02 00:13:05 +0200

    Added Links to Signup and Login from the respective pages

--- frontend/pages/Signup/index.vue
+++ frontend/pages/Signup/index.vue
@@ -1,138 +1,95 @@
+<script setup lang>
+import Card from 'primevue/card'
+import InputText from 'primevue/inputtext'
+import { ref } from 'vue'
+import Password from 'primevue/password'
+
+const client = useSupabaseClient()
+const email = ref(null)
+const password = ref(null)
+const confirmPassword = ref(null)
+const errorMsg = ref(null)
+const successMsg = ref(null)
+
+async function signUp() {
+	try {
+		const { error } = await client.auth.signUp({
+			email: email.value,
+			password: password.value,
+		})
+		if (error) throw error
+		successMsg.value = 'Check your email to confirm your account.'
+	} catch (error) {
+		errorMsg.value = error.value
+	}
+}
+
+definePageMeta({
+	layout: 'auth',
+})
+</script>
+
 <template>
-	<div class="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-		<h1 class="text-3xl font-bold mt- mb-2">Welcome to TerraByte!</h1>
-		<p class="text-lg text-center mb-4">Please sign up using your<br />personal details below</p>
-		<div class="bg-white p-6 rounded-2xl shadow-md w-full max-w-md">
-			<h2 class="text-2xl font-semibold mb-4">Sign up</h2>
-
-			<div class="mb-4">
-				<label for="email" class="block text-sm font-medium text-gray-700">Email Address</label>
-				<InputText
-					id="email"
-					v-model="email"
-					type="email"
-					placeholder="email@example.com"
-					variant="filled"
-					required
-					class="input mt-1 block w-full border-gray-300 rounded-xl text-gray-900 bg-white"
-				/>
-				<small v-if="!email" class="text-red-500 text-sm">Please provide an email address</small>
+	<div class="w-full h-screen flex flex-col justify-center items-center p-4">
+		<img src="~/assets/logo.png" alt="Logo" class="absolute top-2 left-2" />
+		<div class="self-center">
+			<div class="flex flex-col gap-5 items-center w-[400px] max-w-lg">
+				<Card class="w-full border border-surface-border">
+					<template #title>
+						<h1>Sign up</h1>
+					</template>
+					<template #content>
+						<div class="flex flex-col gap-3">
+							<div class="flex flex-col gap-2 items-start">
+								<h3 class="font-semibold">Email Address</h3>
+								<InputText
+									id="email"
+									type="email"
+									class="w-full"
+									v-model="email"
+									placeholder="email@example.com"
+								/>
+							</div>
+							<div class="flex flex-col gap-2 items-start">
+								<h3 class="font-semibold">Password</h3>
+								<div class="card flex justify-center w-full">
+									<Password v-model="password" toggleMask class="w-full">
+										<template #header>
+											<h6 class="font-medium m-0 mb-2 text-base">Pick a password</h6>
+										</template>
+										<template #footer>
+											<Divider />
+											<p class="mt-2 p-0 mb-2">Suggestions</p>
+											<ul class="p-0 pl-2 m-0 ml-2 list-disc leading-6" style="line-height: 1.5">
+												<li>At least one lowercase</li>
+												<li>At least one uppercase</li>
+												<li>At least one numeric</li>
+												<li>Minimum 8 characters</li>
+											</ul>
+										</template>
+									</Password>
+								</div>
+							</div>
+							<div class="flex flex-col gap-2 items-start">
+								<h3 class="font-semibold">Confirm Password</h3>
+								<div class="card flex justify-center w-full">
+									<Password v-model="confirmPassword" toggleMask class="w-full">
+										<template #header>
+											<h6 class="font-medium m-0 mb-2 text-base">Confirm your password</h6>
+										</template>
+									</Password>
+								</div>
+							</div>
+							<small v-if="errorMsg" class="text-red-500">{{ errorMsg }}</small>
+							<small v-if="successMsg" class="text-primary">{{ successMsg }}</small>
+							<Button class="w-full" label="Sing up" @click="signUp" />
+							<small class="text-center"
+								>Already have an account? <NuxtLink to="/login" class="underline">Login </NuxtLink></small
+							>
+						</div>
+					</template>
+				</Card>
 			</div>
-
-			<div class="mb-4">
-				<label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-				<Password
-					id="password"
-					v-model="password"
-					placeholder="Password"
-					feedback
-					:toggleMask="true"
-					promptLabel="Password"
-					weakLabel="Weak Password"
-					mediumLabel="Moderate Password"
-					strongLabel="Strong Password"
-					variant="filled"
-					required
-					:inputStyle="{ backgroundColor: 'white', color: 'black', borderRadius: '0.75rem' }"
-					class="mt-1 block w-full border-gray-300 rounded-xl text-gray-900 bg-white"
-				/>
-				<small v-if="password && passwordErrors.length" class="text-red-500 text-sm">
-					Password must:
-					<ul>
-						<li v-for="(error, index) in passwordErrors" :key="index">{{ error }}</li>
-					</ul>
-				</small>
-			</div>
-
-			<div class="mb-4">
-				<label for="confirmPassword" class="block text-sm font-medium text-gray-700">Confirm Password</label>
-				<Password
-					id="confirmPassword"
-					v-model="confirmPassword"
-					placeholder="Password"
-					:toggleMask="true"
-					promptLabel="Confirm Password"
-					variant="filled"
-					required
-					:inputStyle="{ backgroundColor: 'white', color: 'black', borderRadius: '0.75rem' }"
-					class="mt-1 block w-full border-gray-300 rounded-xl text-gray-900 bg-white"
-				/>
-				<small v-if="confirmPassword && confirmPassword !== password" class="text-red-500 text-sm"
-					>Passwords do not match.</small
-				>
-			</div>
-
-			<Button
-				label="Submit"
-				@click="handleSignUp"
-				:disabled="isFormInvalid"
-				class="w-full bg-primary text-white py-2 rounded-xl mt-4 mb-2"
-			/>
 		</div>
 	</div>
 </template>
-
-<script>
-import Password from 'primevue/password'
-import InputText from 'primevue/inputtext'
-import Button from 'primevue/button'
-
-export default {
-	components: {
-		Password,
-		InputText,
-		Button,
-	},
-	data() {
-		return {
-			email: '',
-			password: '',
-			confirmPassword: '',
-		}
-	},
-	computed: {
-		passwordErrors() {
-			const errors = []
-			if (!/(?=.*[a-z])/.test(this.password)) {
-				errors.push('include at least one lowercase letter')
-			}
-			if (!/(?=.*[A-Z])/.test(this.password)) {
-				errors.push('include at least one uppercase letter')
-			}
-			if (!/(?=.*[0-9])/.test(this.password)) {
-				errors.push('include at least one number')
-			}
-			if (!/(?=.*[!@#$%^&*])/.test(this.password)) {
-				errors.push('include at least one symbol')
-			}
-			if (this.password.length < 8) {
-				errors.push('be at least 8 characters long')
-			}
-			return errors
-		},
-		isPasswordValid() {
-			return this.passwordErrors.length === 0
-		},
-		isFormInvalid() {
-			return !this.email || !this.isPasswordValid || this.password !== this.confirmPassword
-		},
-	},
-	methods: {
-		handleSignUp() {
-			if (this.isFormInvalid) {
-				return
-			}
-			// Handle sign up logic here
-			console.log('Email:', this.email)
-			console.log('Password:', this.password)
-		},
-	},
-}
-</script>
-
-<style scoped>
-.input {
-	background-color: white;
-	color: black;
-}
-</style>
