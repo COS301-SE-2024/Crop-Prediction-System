c30ea2ad8662deffdee7d1e4f1fae4a576d972b4
Xavier893 <xavierrynolds@gmail.com>
2024-06-02 23:41:49 +0200

    Fixed OverlayPanel not showing up to let users log out

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,21 +1,27 @@
 <template>
 	<nav class="p-2 px-5 z-50 w-full shadow-md dark:bg-surface-900">
 		<div class="flex justify-between xl:justify-normal xl:grid xl:grid-cols-3 h-full w-full">
-			<Sidebar class="justify-self-start"/>
-            <NuxtLink to="/" class="text-2xl font-[500] text-primary-500 justify-self-center self-center h-full">
-                <img src="../assets/logo.png" alt="Logo" class="w-36 xl:h-14 xl:w-auto object-cover dark:hidden block" />
-                <img src="../assets/logo-alt.png" alt="Logo" class="w-36 xl:h-14 xl:w-auto object-cover hidden dark:block" />
-            </NuxtLink>
+			<Sidebar class="justify-self-start" />
+			<NuxtLink to="/" class="text-2xl font-[500] text-primary-500 justify-self-center self-center h-full">
+				<img src="../assets/logo.png" alt="Logo" class="w-36 xl:h-14 xl:w-auto object-cover dark:hidden block" />
+				<img src="../assets/logo-alt.png" alt="Logo" class="w-36 xl:h-14 xl:w-auto object-cover hidden dark:block" />
+			</NuxtLink>
+			<OverlayPanel ref="op" class="w-60 flex flex-col justify-center items-center">
+				<div class="flex flex-col items-center justify-center gap-5">
+					<h3>{{ user.email }}</h3>
+					<Button label="Log Out" severity="danger" class="w-full" @click="signOut" />
+				</div>
+			</OverlayPanel>
 			<div class="flex flex-row items-center justify-end gap-4 justify-self-end dark:text-white">
-                <div class="p-5 sm:p-0">
-                    <i class="pi pi-bell" style="font-size: 1.5rem" />
-                </div>
-                <div class="hidden sm:block">
-                    <i class="pi pi-user" @click="toggle" style="font-size: 1.5rem" />
-                </div>
-                <div class="hidden sm:block">
-                    <i class="pi pi-cog" style="font-size: 1.5rem" />
-                </div>
+				<div class="p-5 sm:p-0">
+					<i class="pi pi-bell" style="font-size: 1.5rem" />
+				</div>
+				<div class="hidden sm:block">
+					<i class="pi pi-user" @click="toggle" style="font-size: 1.5rem" />
+				</div>
+				<div class="hidden sm:block">
+					<i class="pi pi-cog" style="font-size: 1.5rem" />
+				</div>
 			</div>
 		</div>
 	</nav>
