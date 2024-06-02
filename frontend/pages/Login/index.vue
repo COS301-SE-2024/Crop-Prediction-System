25b9c091bc2290c6aba7358fccab23f51221e656
SMangera8 <111564099+SMangera8@users.noreply.github.com>
2024-06-02 18:31:23 +0200

    Update Login Form to use Card

--- frontend/pages/Login/index.vue
+++ frontend/pages/Login/index.vue
@@ -1,112 +1,73 @@
-<template>
-	<div class="min-h-screen flex flex-col justify-center items-center">
-		<h1 class="text-3xl font-bold mt-4 mb-2">Welcome to TerraByte!</h1>
-		<p class="text-md text-center mb-4">Please login using your<br />personal details below.</p>
-		<div class="border border-surface-border bg-white p-6 rounded-2xl shadow-md w-full max-w-md">
-			<h2 class="text-2xl font-semibold mb-4">Log in</h2>
-
-			<div class="mb-4">
-				<label for="email" class="block text-sm font-medium text-gray-700">Email Address</label>
-				<InputText id="email" v-model="email" type="email" placeholder="email@example.com" class="w-full" />
-			</div>
-
-			<div class="mb-4">
-				<label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-				<Password
-					id="password"
-					v-model="password"
-					placeholder="Password"
-					feedback
-					:toggleMask="true"
-					promptLabel="Password"
-					weakLabel="Weak Password"
-					mediumLabel="Moderate Password"
-					strongLabel="Strong Password"
-					required
-					class="mt-1 block w-full border-gray-300 rounded-md text-gray-900 bg-white"
-				/>
-				<small v-if="loginError" class="text-red-500 text-sm">Incorrect Email Address or Password</small>
-			</div>
-			<small v-if="errorMsg" class="text-red-500">{{ errorMsg }}</small>
-			<Button label="Login" class="w-full" @submit.prevent="signIn" @click="signIn" />
-			<small class="text-center">Dont have an account? <NuxtLink to="/signup" class="underline">Sign up</NuxtLink></small>
-		</div>
-	</div>
-	<img src="~/assets/logo.png" alt="Logo" class="absolute top-2 left-2" />
-</template>
-
-<script setup>
+<script setup lang>
+import Card from 'primevue/card'
 import Password from 'primevue/password'
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
 import { ref } from 'vue'
 
+const client = useSupabaseClient()
 const email = ref(null)
 const password = ref(null)
 const errorMsg = ref(null)
 
-definePageMeta({
-	layout: 'auth',
-})
-
-const client = useSupabaseClient()
 const signIn = async () => {
-	try {
-		const { error } = await client.auth.signInWithPassword({
-			email: email.value,
-			password: password.value,
-		})
-		if (error) throw error
-		navigateTo('/')
-	} catch (error) {
-		errorMsg.value = error.message
-	}
+  try {
+	const { error } = await client.auth.signInWithPassword({
+	  email: email.value,
+	  password: password.value,
+	})
+	if (error) throw error
+	navigateTo('/')
+  } catch (error) {
+	errorMsg.value = error.message
+  }
 }
 
-function isFormInvalid() {
-	return !this.email || !this.password
-}
-
-// export default {
-//   components: {
-//     Password,
-//     InputText,
-//     Button,
-//   },
-//   data() {
-//     return {
-//       email: '',
-//       password: '',
-//       confirmPassword: '',
-//       loginError: false,
-//     }
-//   },
-//   computed: {
-//     isFormInvalid() {
-//       return !this.email || !this.password
-//     },
-//   },
-//   methods: {
-//     handleLogin() {
-//       if (!this.email) {
-//         return // Exit early if email is not provided
-//       }
-//
-//       // Simulate login process
-//       if (this.email === 'correct@email.com' && this.password === 'correctpassword') {
-//         console.log('Login successful')
-//         // Redirect or perform action for successful login
-//       } else {
-//         this.loginError = true // Set login error to true
-//       }
-//     },
-//   },
-// }
+definePageMeta({
+  layout: 'auth',
+})
 </script>
 
-<style scoped>
-.input {
-	background-color: white;
-	color: black;
-}
-</style>
+<template>
+	<div class="w-full h-screen flex flex-col justify-center items-center p-4">
+	  <img src="~/assets/logo.png" alt="Logo" class="absolute top-2 left-2" />
+	  <div class="self-center">
+		<div class="flex flex-col gap-5 items-center w-[400px] max-w-md">
+		  <Card class="w-full border border-surface-border">
+			<template #title>
+			  <h1>Log in</h1>
+			</template>
+			<template #content>
+			  <div class="flex flex-col gap-3">
+				<div class="flex flex-col gap-2 items-start">
+				  <h3 class="font-semibold">Email Address</h3>
+				  <InputText
+					id="email"
+					type="email"
+					class="w-full"
+					v-model="email"
+					placeholder="email@example.com"
+				  />
+				</div>
+				<div class="flex flex-col gap-2 items-start">
+				  <h3 class="font-semibold">Password</h3>
+				  <div class="card flex justify-center w-full">
+					<Password v-model="password" toggleMask class="w-full">
+					  <template #header>
+						<h6 class="font-medium m-0 mb-2 text-base">Enter your password</h6>
+					  </template>
+					</Password>
+				  </div>
+				</div>
+				<small v-if="errorMsg" class="text-red-500">{{ errorMsg }}</small>
+				<Button class="w-full" label="Login" @click="signIn" />
+				<small class="text-center">Don't have an account? <NuxtLink to="/signup" class="underline">Sign up</NuxtLink></small>
+			  </div>
+			</template>
+		  </Card>
+		</div>
+	  </div>
+	</div>
+</template>
+  
+  
