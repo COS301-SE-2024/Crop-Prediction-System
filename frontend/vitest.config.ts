e9f43cb023a4b290c2caba954d774be2f07ccf13
Xavier893 <xavierrynolds@gmail.com>
2024-06-03 08:55:07 +0200

    Added dependencies

---
+++ frontend/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+
+export default defineVitestConfig({
+  test: {
+    environmentOptions: {
+      nuxt: {
+        mock: {
+          intersectionObserver: true,
+          indexedDb: true,
+        },
+      }
+    },
+    testTimeout: 10000,
+  }
+})
+
