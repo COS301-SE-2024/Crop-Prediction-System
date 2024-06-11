b07abf59dfea452bf8176a0b25d0f96bd9908d23
Iwan de Jong <idj.idejong@gmail.com>
2024-06-11 09:59:34 +0200

    ndvi images ready to be processed

--- backend/model/ndvi_raster_all.py
+++ backend/model/ndvi_raster_all.py
@@ -5,6 +5,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import scipy as sp
+
+# # Supabase
+# from dotenv import load_dotenv
+# from supabase import create_client, Client
+
+# import logging
+
+# # Load environment variables
+# load_dotenv()
+
+# # Get Supabase URL and Key
+# supabase_url = os.environ.get("SUPABASE_URL")
+# supabase_key = os.environ.get("SUPABASE_KEY")
+
+# # Create Supabase client
+# supabase_client = create_client(supabase_url, supabase_key)
 
 # Define function to process each NDVI image
 def process_ndvi_image(file_path, x, y, width, height):
@@ -34,18 +51,37 @@ def process_ndvi_image(file_path, x, y, width, height):
     max_normalized_ndvi = normalized_roi.max()
     min_normalized_ndvi = normalized_roi.min()
 
+    # Additional statistics for the normalized ROI
+    variance_normalized_ndvi = np.var(normalized_roi)
+    iqr_normalized_ndvi = np.percentile(normalized_roi, 75) - np.percentile(normalized_roi, 25)
+    mode_normalized_ndvi = sp.stats.mode(normalized_roi)
+
+    valid_pixels = np.count_nonzero(~np.isnan(normalized_roi))
+
+    # Timestamp of the NDVI image
+    # filename: sa2207.tif
+    date_str = os.path.basename(file_path).split('.')[0]
+    year = date_str[2:4]
+    sample = date_str[4:]
+    date = f'20{year}-01-01'
+    date = pd.to_datetime(date) + pd.DateOffset(days=int(sample) * 365 / 72)
+
+    print(date)
+
+
     return {
         'file_path': file_path,
-        # 'mean_ndvi': mean_ndvi,
-        # 'median_ndvi': median_ndvi,
-        # 'std_dev_ndvi': std_dev_ndvi,
-        # 'max_ndvi': max_ndvi,
-        # 'min_ndvi': min_ndvi,
-        'mean_normalized_ndvi': mean_normalized_ndvi,
-        'median_normalized_ndvi': median_normalized_ndvi,
-        'std_dev_normalized_ndvi': std_dev_normalized_ndvi,
-        'max_normalized_ndvi': max_normalized_ndvi,
-        'min_normalized_ndvi': min_normalized_ndvi
+        'date': date,
+        'mean_normalized_ndvi': float(mean_normalized_ndvi),
+        'median_normalized_ndvi': float(median_normalized_ndvi),
+        'std_dev_normalized_ndvi': float(std_dev_normalized_ndvi),
+        'max_normalized_ndvi': float(max_normalized_ndvi),
+        'min_normalized_ndvi': float(min_normalized_ndvi),
+        'variance_normalized_ndvi': float(variance_normalized_ndvi),
+        'iqr_normalized_ndvi': float(iqr_normalized_ndvi),
+        'mode_normalized_ndvi': float(mode_normalized_ndvi[0]),
+        'mode_normalized_ndvi_count': float(mode_normalized_ndvi[1]),
+        'valid_pixels': int(valid_pixels),
     }
 
 # Define ROI coordinates (x, y, width, height)
@@ -63,13 +99,39 @@ ndvi_files = [os.path.join(ndvi_dir, f) for f in os.listdir(ndvi_dir) if f.endsw
 # Process each NDVI image file and store statistics in a list
 ndvi_data = []
 for file_path in ndvi_files:
-    ndvi_data.append(process_ndvi_image(file_path, x, y, width, height))
+    # Process the NDVI image and store the extracted data
+    try:
+        processed_data = process_ndvi_image(file_path, x, y, width, height)
+
+        processed_data['date'] = processed_data['date'].isoformat()
+
+        ndvi_data.append(processed_data)
+
+        # response = supabase_client.schema('public').table('ndvi').insert([{
+        #     "date": processed_data['date'],
+        #     "file_path": processed_data['file_path'],
+        #     "mean": processed_data['mean_normalized_ndvi'],
+        #     "median": processed_data['median_normalized_ndvi'],
+        #     "stdev": processed_data['std_dev_normalized_ndvi'],
+        #     "var": processed_data['variance_normalized_ndvi'],
+        #     "iqr": processed_data['iqr_normalized_ndvi'],
+        #     "mode": processed_data['mode_normalized_ndvi'],
+        #     "mode_count": processed_data['mode_normalized_ndvi_count'],
+        #     "valid_pixels": processed_data['valid_pixels'],
+        # }]).execute()
+
+        # logging.info(f"Supabase response: {response}")
+    except Exception as e:
+        print(e)
 
 # Create a DataFrame from the extracted data
 ndvi_df = pd.DataFrame(ndvi_data)
 
 # Print the DataFrame to verify
 print(ndvi_df)
+
+# Sort the DataFrame by date
+ndvi_df = ndvi_df.sort_values('date')
 
 # Optionally, you can save the DataFrame to a CSV file
 ndvi_df.to_csv('ndvi_local_all.csv', index=False)
--- backend/model/ndvi_raster_batch.py
+++ backend/model/ndvi_raster_batch.py
@@ -10,6 +10,22 @@ import os
 import pandas as pd
 from tqdm import tqdm
 import re
+from scipy import stats as sp
+
+# Supabase
+import supabase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Supabase credentials
+supabase_url = os.environ.get('SUPABASE_URL')
+supabase_key = os.environ.get('SUPABASE_KEY')
+
+# Initialize Supabase client
+supabase_client = supabase.create_client(supabase_url, supabase_key)
+
+# 72 samples per year. Write a line to add the date to the dataframe (e.g. 2022-01-01). The file name is 2207 which means the 7th sample of 2022. This correlates to about 365/72*7 = 35 days after the start of the year.
 
 # Define function to process each NDVI image
 def process_ndvi_image(file_path, x, y, width, height):
@@ -35,23 +51,39 @@ def process_ndvi_image(file_path, x, y, width, height):
     # Calculate statistics for the normalized ROI
     mean_normalized_ndvi = normalized_roi.mean()
     median_normalized_ndvi = np.median(normalized_roi)
+    mode_normalized_ndvi = sp.mode(normalized_roi)
+    variance_normalized_ndvi = np.var(normalized_roi)
     std_dev_normalized_ndvi = normalized_roi.std()
     max_normalized_ndvi = normalized_roi.max()
     min_normalized_ndvi = normalized_roi.min()
+    iqr_normalized_ndvi = np.percentile(normalized_roi, 75) - np.percentile(normalized_roi, 25)
+
+    # Count of valid pixels
+    valid_pixels = np.count_nonzero(~np.isnan(normalized_roi))
+
+    # Timestamp of the NDVI image
+    date_str = os.path.basename(file_path).split('.')[0]
+    year = date_str[2:4]
+    sample = date_str[4:]
+    date = f'20{year}-01-01'
+    date = pd.to_datetime(date) + pd.DateOffset(days=int(sample) * 365 / 72)
+
+    # Print confirmation of ndvi image processing
+    print("Processed NDVI image: ", file_path, "\n",)
 
     return {
+        'date': date,
         'file_path': file_path,
-        # 'mean_ndvi': mean_ndvi,
-        # 'median_ndvi': median_ndvi,
-        # 'std_dev_ndvi': std_dev_ndvi,
-        # 'max_ndvi': max_ndvi,
-        # 'min_ndvi': min_ndvi,
         'mean_normalized_ndvi': mean_normalized_ndvi,
         'median_normalized_ndvi': median_normalized_ndvi,
         'std_dev_normalized_ndvi': std_dev_normalized_ndvi,
         'max_normalized_ndvi': max_normalized_ndvi,
-        'min_normalized_ndvi': min_normalized_ndvi
+        'min_normalized_ndvi': min_normalized_ndvi,
         # TODO: Make a decision on which statistics to include in the final DataFrame
+        'mode_normalized_ndvi': mode_normalized_ndvi,
+        'variance_normalized_ndvi': variance_normalized_ndvi,
+        'iqr_normalized_ndvi': iqr_normalized_ndvi,
+        'valid_pixels': valid_pixels,
     }
 
 # Define ROI coordinates (x, y, width, height)
@@ -98,13 +130,39 @@ for zip_url in tqdm(ndvi_zip_files, desc="Downloading and Processing", unit="zip
         for file_name in zip_ref.namelist():
             if file_name.endswith('.tif'):
                 file_path = os.path.join('temp_folder', file_name)
-                ndvi_data.append(process_ndvi_image(file_path, x, y, width, height))
+                
+                # Process the NDVI image
+                processed_data = process_ndvi_image(file_path, x, y, width, height)
+
+                # Append the processed data to the list
+                ndvi_data.append(processed_data)
+
+                # Add to Supabase
+                supabase_client.table('historical.ndvi').insert([{
+                    'date': processed_data['date'],
+                    'file_path': processed_data['file_path'],
+                    'mean_normalized_ndvi': processed_data['mean_normalized_ndvi'],
+                    'median_normalized_ndvi': processed_data['median_normalized_ndvi'],
+                    'std_dev_normalized_ndvi': processed_data['std_dev_normalized_ndvi'],
+                    'max_normalized_ndvi': processed_data['max_normalized_ndvi'],
+                    'min_normalized_ndvi': processed_data['min_normalized_ndvi'],
+                    'mode_normalized_ndvi': processed_data['mode_normalized_ndvi'],
+                    'variance_normalized_ndvi': processed_data['variance_normalized_ndvi'],
+                    'iqr_normalized_ndvi': processed_data['iqr_normalized_ndvi'],
+                    'valid_pixels': processed_data['valid_pixels'],
+                }]).execute()
+
+                # Remove tif file after processing
+                os.remove(file_path)
 
 # Create a DataFrame from the extracted data
 ndvi_df = pd.DataFrame(ndvi_data)
 
 # Print the DataFrame to verify
 print(ndvi_df)
+
+# Sort the DataFrame by date
+ndvi_df = ndvi_df.sort_values('date')
 
 # Optionally, you can save the DataFrame to a CSV file
 ndvi_df.to_csv('ndvi_statistics.csv', index=False)
