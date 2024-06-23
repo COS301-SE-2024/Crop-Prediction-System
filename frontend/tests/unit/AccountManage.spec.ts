f9377e4c3e8b7c4a9071ee8007f1fd9fb13a1d8a
SW1F7YY <stephansmit112@gmail.com>
2024-06-23 11:51:50 +0200

    added render test for account

--- frontend/tests/unit/AccountManage.spec.ts
+++ frontend/tests/unit/AccountManage.spec.ts
@@ -2,13 +2,20 @@
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import { it, expect, describe } from 'vitest'
 import AccountManage from '~/components/AccountManage.vue'
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount,mount } from '@vue/test-utils'
 import { vi } from 'vitest'
+import { resolve } from 'chart.js/helpers'
 
 describe('AccountManage', () => {
     it('can mount the component', async () => {
         const component = await mountSuspended(AccountManage)
         expect(component.exists()).toBe(true)
     })
+    it('renders the div for toggling', async () => {
+        const wrapper = await mountSuspended(AccountManage)
+        const onCLickToggle = wrapper.find('div[class="w-full flex flex-row gap-3 items-center dark:hover:bg-surface-400/10 hover:bg-surface-100 cursor-pointer p-2 rounded-lg"]')
+        expect(onCLickToggle.exists()).toBe(true)
+    })
+    
 })
 
