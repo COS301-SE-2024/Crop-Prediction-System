744008d2795be2a858a2363f0b3797cbff8ae38e
XavRyn <u20526254@tuks.co.za>
2024-06-23 23:01:53 +0200

    Added openweather api call endpoint

--- frontend/server/api/getWeather.ts
+++ frontend/server/api/getWeather.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler(async (event) => {
 	const proxyUrl = useRuntimeConfig().public.openweatherApiUrl
-  const apiKey = useRuntimeConfig().public.openweatherApiKey
-  const lat = getQuery(event).lat
-  const lon = getQuery(event).lon
+	const apiKey = useRuntimeConfig().public.openweatherApiKey
+	const lat = getQuery(event).lat
+	const lon = getQuery(event).lon
 
 	const apiUrl = `${proxyUrl}/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
 
