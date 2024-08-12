63e2e419ef360099f69e59f8889f6e4cbcc56125
SW1F7YY <stephansmit112@gmail.com>
2024-08-12 14:43:16 +0200

    started with additional testing and the receving of data

--- frontend/tests/integration/GetFields.spec.ts
+++ frontend/tests/integration/GetFields.spec.ts
@@ -6,15 +6,9 @@ import FieldData from '~/components/FieldData.vue'
 vi.mock('$fetch', () => vi.fn());
 
 describe('check the field data', () => {
-    it('should return the correct field data', async () => {
-        const mockFieldData = {
-            field_name: 'test',
-            crop_type: 'wheat',
-            hectare: '0.211313511746061',
-            field_area: '[[25.746, 28.228], [25.748, 28.230], [25.749, 28.231], [25.746, 28.228]]',
-        };
-
-        const fetchSpy = vi.spyOn(globalThis, '$fetch').mockResolvedValue(mockFieldData);
+    it('the api call should occur when the component is mounted', async () => {
+        const userIDcommon = "02dffef4-7788-423c-869e-17db9c821542";
+        const fetchSpy = vi.spyOn(globalThis, '$fetch').mockResolvedValue(userIDcommon);
         const userID = "02dffef4-7788-423c-869e-17db9c821542";
         const wrapper = mount(FieldData, {
             global:{
@@ -32,5 +26,21 @@ describe('check the field data', () => {
             // undefined since the user isnt logged in
             expect.objectContaining({params: {userid: undefined}}),
         );
+    })
+
+    it("gets the field data correctly", async () => {
+        const mockFieldData = {
+            field_name: 'test',
+            crop_type: 'wheat',
+            hectare: '0.211313511746061',
+            field_area: '[[25.746, 28.228], [25.748, 28.230], [25.749, 28.231], [25.746, 28.228]]',
+        };
+        vi.spyOn(globalThis, '$fetch').mockResolvedValue(mockFieldData);
+        let userID = '02dffef4-7788-423c-869e-17db9c821542';
+        // fetch('/api/getUserFields', {
+        //     params: { userid: userID },
+        //   })
+        //     .then((response) => response.json())
+        
     })
 })
