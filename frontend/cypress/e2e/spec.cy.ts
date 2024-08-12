4de29920f03bec49b47af4fdec17e3822f0afc47
SMangera8 <u22505319@tuks.co.za>
2024-08-12 15:10:32 +0200

    Cypress Test For Demo

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,103 +1,26 @@
-describe('Signup Page', () => {
-  beforeEach(() => {
-    cy.visit('/signup')
+describe.only('Demo', () => {
+
+  before(() => {
+    cy.login('ggu.capstone@gmail.com', 'Test1234#')
+    cy.visit('/')
   })
 
-  it('should display the signup form', () => {
-    cy.get('h1').should('contain', 'Sign up')
-    cy.get('#email').should('be.visible')
-    cy.get('input[type="password"]').should('have.length', 2)
+  it('Demo', () => {
+
+    cy.get('.w-full.md\\:w-1\\/3').should('exist')
+    cy.get('.w-full.md\\:w-2\\/3').should('exist')
+
+    cy.get('#pv_id_1').click()
+    cy.get('#pv_id_1_list').should('be.visible')
+    cy.get('#pv_id_1_list').contains('Wheat 3').click()
+    cy.get('legend').click()
+
+    cy.get('i.pi.pi-user').click()
+    cy.get('#pv_id_5_3').click()
+    cy.get('#pv_id_5_3').click()
+    cy.get('#pv_id_5_6').click()
+    
   })
+});
 
-  it('should enable the sign up button when passwords match', () => {
-    const email = `test${Date.now()}@example.com`
-    const password = 'Password123!'
-
-    cy.get('#email').clear().type(email)
-
-    // Ensure the first password input is visible and not obstructed
-    cy.get('input[type="password"]').first().should('be.visible').clear().type(password)
-
-    // Click outside to remove any covering elements
-    cy.get('body').click(0, 0)
-
-    // Ensure the second password input is visible and not obstructed
-    cy.get('input[type="password"]').last().should('be.visible').clear().type(password)
-    cy.get('button').contains('Sign up').should('not.be.disabled')
-  })
-
-  it('should sign up successfully', () => {
-    const email = `test${Date.now()}@example.com`
-    const password = 'Password123!'
-
-    cy.get('#email').clear().type(email)
-
-    // Ensure the first password input is visible and not obstructed
-    cy.get('input[type="password"]').first().should('be.visible').clear().type(password)
-
-    // Click outside to remove any covering elements
-    cy.get('body').click(0, 0)
-
-    // Ensure the second password input is visible and not obstructed
-    cy.get('input[type="password"]').last().should('be.visible').clear().type(password)
-
-    cy.get('button').contains('Sign up').click()
-
-    cy.get('small').contains('Check your email to confirm your account.').should('be.visible')
-  })
-
-  it('should sign in with Google OAuth', () => {
-      cy.get('button')  // Use a more specific selector if necessary
-      .find('span.pi.pi-google')  // Find the icon
-      .should('be.visible')        // Ensure the icon is visible
-      .click({ force: true }) 
-  })
-
-  it('should navigate to the login page', () => {
-      cy.get('small')
-        .contains('Already have an account?')
-        .find('a')  // Find the link element
-        .should('have.attr', 'href', '/login')  // Check that the href attribute is correct
-        .click()  // Click the link
-      cy.url().should('include', '/login')  // Verify that the URL includes /login
-  })
-})
-
-//====================================================================================================================================//
-
-describe('Login Page', () => {
-  beforeEach(() => {
-    cy.visit('/login')
-  })
-
-  it('should display the login form', () => {
-    cy.get('h1').should('contain', 'Log in')
-    cy.get('#email').should('be.visible')
-    cy.get('input[type="password"]').should('be.visible')
-    cy.get('button').contains('Login').should('be.visible')
-  })
-
-  it('should show error message with incorrect login', () => {
-    const email = `test${Date.now()}@example.com`
-    const password = 'WrongPassword'
-
-    cy.get('#email').clear().type(email)
-    cy.get('input[type="password"]').clear().type(password)
-    cy.get('button').contains('Login').click()
-
-    cy.get('small').contains('Invalid login credentials').should('be.visible')
-  })
-
-  // it('should login successfully', () => {
-  //   const email = `test${Date.now()}@example.com`
-  //   const password = 'Password123!'
-
-  //   cy.get('#email').clear().type(email)
-  //   cy.get('input[type="password"]').clear().type(password)
-  //   cy.get('button').contains('Login').click()
-
-  //   // Adjust based on the actual redirection
-  //   cy.url().should('include', '/confirm') 
-  // })
-})
 
---
+++ frontend/cypress/support/index.d.ts
@@ -0,0 +1,16 @@
+// cypress/support/index.d.ts
+
+if (process.env.NODE_ENV === 'test') {
+  window.__VUE_DEVTOOLS_GLOBAL_HOOK__ = {
+    Vue: null,
+  }
+}
+
+declare namespace Cypress {
+    interface Chainable {
+      login(email: string, password: string): Chainable<void>
+    }
+  }
+
+
+  
