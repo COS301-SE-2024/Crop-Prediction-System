8c05d695a2a27d616e0062e4fb0ce55316232079
Iwan de Jong <idj.idejong@gmail.com>
2024-08-12 09:13:26 +0200

    fix module imports

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -40,7 +40,7 @@ def test_create_field():
     }
 
 def test_get_field_info():
-    with patch('database.supabaseFunctions.supabaseFunctions.getFieldInfo') as mock_getFieldInfo:
+    with patch('backend.database.supabaseFunctions.supabaseFunctions.getFieldInfo') as mock_getFieldInfo:
         mock_getFieldInfo.return_value = {
             "id": field_id,
             "field_area": [
@@ -62,7 +62,7 @@ def test_get_field_info():
         assert response.json()["team_id"] == team_id
 
 def test_get_field_data():
-    with patch('database.supabaseFunctions.supabaseFunctions.getFieldData') as mock_getFieldData:
+    with patch('backend.database.supabaseFunctions.supabaseFunctions.getFieldData') as mock_getFieldData:
         mock_getFieldData.return_value = {
             "field_id": field_id,
             "field_name": "TEST",
@@ -90,21 +90,21 @@ def test_get_field_data():
         assert response.json()[0]["date"] == f"{today}"
 
 # def test_fetch_weather():
-#     with patch('database.supabaseFunctions.supabaseFunctions.fetchWeatherForAllFields') as mock_fetchWeather:
+#     with patch('backend.database.supabaseFunctions.supabaseFunctions.fetchWeatherForAllFields') as mock_fetchWeather:
 #         mock_fetchWeather.return_value = {"success": "Weather fetched for all fields"}
 #         response = client.get("/fetchWeather")
 #         assert response.status_code == 200
 #         assert response.json() == mock_fetchWeather.return_value
 
 # def test_fetch_summary():
-#     with patch('database.supabaseFunctions.supabaseFunctions.fetchSummary') as mock_fetchSummary:
+#     with patch('backend.database.supabaseFunctions.supabaseFunctions.fetchSummary') as mock_fetchSummary:
 #         mock_fetchSummary.return_value = {"success": "Summary fetched for all fields"}
 #         response = client.get("/fetchSummary")
 #         assert response.status_code == 200
 #         assert response.json() == mock_fetchSummary.return_value
 
 def test_get_team_field_data():
-    with patch('database.supabaseFunctions.supabaseFunctions.getTeamFieldData') as mock_getTeamFieldData:
+    with patch('backend.database.supabaseFunctions.supabaseFunctions.getTeamFieldData') as mock_getTeamFieldData:
         mock_getTeamFieldData.return_value = {
             "field_id": field_id,
             "field_name": "TEST",
@@ -114,7 +114,7 @@ def test_get_team_field_data():
         assert len(response.json()) == 1
 
 def test_get_team_fields():
-    with patch('database.supabaseFunctions.supabaseFunctions.getTeamFields') as mock_getUserFields:
+    with patch('backend.database.supabaseFunctions.supabaseFunctions.getTeamFields') as mock_getUserFields:
         mock_getUserFields.return_value = [{"field_id": field_id, "field_name": "TEST"}]
         response = client.get(f"/getTeamFields?team_id={team_id}")
         data = response.json()
@@ -122,28 +122,28 @@ def test_get_team_fields():
         assert any(item['id'] == field_id for item in data)
 
 # def test_add_to_team():
-#     with patch('database.supabaseFunctions.supabaseFunctions.addToTeam') as mock_addToTeam:
+#     with patch('backend.database.supabaseFunctions.supabaseFunctions.addToTeam') as mock_addToTeam:
 #         mock_addToTeam.return_value = {"success": "User added to team"}
 #         response = client.post("/addToTeam", json={"team_id": team_id, "user_id": user_id})
 #         assert response.status_code == 200
 #         assert response.json() == {"success": "User added to team"}
 
 # def test_remove_from_team():
-#     with patch('database.supabaseFunctions.supabaseFunctions.removeFromTeam') as mock_removeFromTeam:
+#     with patch('backend.database.supabaseFunctions.supabaseFunctions.removeFromTeam') as mock_removeFromTeam:
 #         mock_removeFromTeam.return_value = {"success": "User removed from team"}
 #         response = client.get(f"/removeFromTeam?user_id={user_id}")
 #         assert response.status_code == 200
 #         assert response.json() == {"success": "User removed from team"}
 
 # def test_update_roles():
-#     with patch('database.supabaseFunctions.supabaseFunctions.updateRoles') as mock_updateRoles:
+#     with patch('backend.database.supabaseFunctions.supabaseFunctions.updateRoles') as mock_updateRoles:
 #         mock_updateRoles.return_value = {"success": "Roles updated"}
 #         response = client.post("/updateRoles", json={"user_id": user_id, "roles": ["admin"]})
 #         assert response.status_code == 200
 #         assert response.json() == {"success": "Roles updated"}
 
 def test_get_team_id():
-    with patch('database.supabaseFunctions.supabaseFunctions.getTeamId') as mock_getTeamId:
+    with patch('backend.database.supabaseFunctions.supabaseFunctions.getTeamId') as mock_getTeamId:
         mock_getTeamId.return_value = {"team_id": team_id}
         response = client.get(f"/getTeamId?user_id={user_id}")
         print(response.json())
@@ -151,7 +151,7 @@ def test_get_team_id():
         assert response.json() == {"team_id": team_id}
 
 def test_update_field():
-    with patch('database.supabaseFunctions.supabaseFunctions.updateField') as mock_updateField:
+    with patch('backend.database.supabaseFunctions.supabaseFunctions.updateField') as mock_updateField:
         mock_updateField.return_value = {
             "status": "success",
             "data": {
@@ -199,7 +199,7 @@ def test_update_field():
         assert response.json()["data"]["crop_type"] == "maize"
 
 # def test_update_entry():
-#     with patch('database.supabaseFunctions.supabaseFunctions.updateEntry') as mock_updateEntry:
+#     with patch('backend.database.supabaseFunctions.supabaseFunctions.updateEntry') as mock_updateEntry:
 #         mock_updateEntry.return_value = {
 #             "field_id": field_id,
 #             "date": today,
@@ -263,7 +263,7 @@ def test_update_field():
 #         assert response.json()["data"]["rain"] == 100
 
 def test_delete_field():
-    with patch('database.supabaseFunctions.supabaseFunctions.deleteField') as mock_deleteField:
+    with patch('backend.database.supabaseFunctions.supabaseFunctions.deleteField') as mock_deleteField:
         mock_deleteField.return_value = {
             "status": "success",
             "data": {
