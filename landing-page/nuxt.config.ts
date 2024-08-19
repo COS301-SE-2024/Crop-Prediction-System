f6267ef1dc33df6ef50b865c6dba7f06221c220b
Tiaan Pouwels <97939187+Tiaan2@users.noreply.github.com>
2024-08-19 23:41:53 +0200

    Nuxt config setup and added primevue with themes

--- landing-page/nuxt.config.ts
+++ landing-page/nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+	css: ['./assets/css/tailwind.css', 'primeicons/primeicons.css'],
+	devtools: { enabled: true },
+	modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'nuxt-primevue', '@nuxtjs/supabase', '@vueuse/nuxt'],
+	colorMode: {
+		classSuffix: '',
+	},
+	primevue: {
+		options: {
+			unstyled: true,
+		},
+		importPT: { from: path.resolve(__dirname, './presets/lara/') },
+	},
+	components: {
+		global: true,
+		dirs: ['`/components'],
+	},
 })
