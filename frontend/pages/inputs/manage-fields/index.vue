9858ab423fa5ff86bdfa925b91b838ed67d2fbc9
Xavier893 <xavierrynolds@gmail.com>
2024-08-27 23:27:53 +0200

    Refactored page to be more user friendly. Introduces search field by name and filter by crop type

--- frontend/pages/inputs/manage-fields/index.vue
+++ frontend/pages/inputs/manage-fields/index.vue
@@ -1,49 +1,139 @@
 <template>
-	<div class="h-full w-full flex flex-col gap-5">
-		<div class="flex flex-row justify-between items-center _mb-4">
-			<span class="font-bold text-xl dark:text-white">Fields</span>
-			<Button label="Add Field" @click="isDialogVisible = true" />
-		</div>
-
-		<Dialog v-model:visible="isDialogVisible" header="Add Field" :modal="true" :style="{ width: '450px' }">
-			<div class="flex flex-col space-y-2 mb-4">
-				<InputText v-model="fieldName" placeholder="Field Name" />
-				<Dropdown v-model="selectedCropType" :options="cropOptions" optionLabel="name" placeholder="Crop Type" />
-			</div>
-			<template #footer>
-				<Button label="Cancel" severity="danger" icon="pi pi-times" @click="cancelAddField" />
-				<Button label="Save" icon="pi pi-check" @click="proceedToDrawing" />
-			</template>
-		</Dialog>
-
-		<ConfirmDialog />
-		<div class="p-3 border border-surface-300 rounded-md dark:border-surface-600">
-			<GoogleMap
-				class="h-[40rem]"
-				ref="googleMapRef"
-				:isDrawingEnabled="isDrawingEnabled"
-				@polygonDrawn="handlePolygonDrawn"
+	<div class="h-full w-full flex flex-col items-center justify-center gap-5">
+		<div v-show="!isLoading" class="flex flex-col sm:flex-row justify-start items-center gap-4 mt-4 w-full">
+			<InputText v-model="searchQuery" placeholder="Search Field Name..." class="w-full sm:w-[375px]" />
+			<Dropdown
+				v-model="selectedCropType"
+				:options="cropOptions"
+				optionLabel="name"
+				placeholder="Filter by Crop Type"
+				class="w-full sm:w-[250px]"
+				showClear
 			/>
 		</div>
+
+		<div v-if="isLoading" class="w-full h-full mt-48 gap-5 flex flex-col items-center justify-center">
+			<ProgressSpinner />
+			<h2 class="dark:text-white font-bold">Fetching Fields...</h2>
+		</div>
+
+		<div v-else class="grid lg:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 w-full">
+			<Card v-for="field in filteredFields" :key="field.id">
+				<template #header>
+					<div class="text-center p-2">
+						<h2 class="text-2xl font-bold">{{ field.field_name }}</h2>
+					</div>
+				</template>
+				<template #content>
+					<div class="text-gray-600 dark:text-gray-300 flex flex-col justify-between items-start gap-2">
+						<span> <strong>Crop Type: </strong>{{ capitalizeFirstCharacter(field.crop_type) }}</span>
+						<span><strong>Field Size: </strong>{{ field.hectare.toFixed(2) }}ha</span>
+						<span><strong>Field ID: </strong>{{ field.id }}</span>
+					</div>
+				</template>
+				<template #footer>
+					<div class="flex gap-3">
+						<Button label="Delete" severity="danger" size="small" outlined class="w-full" />
+						<Button label="View or Edit" size="small" class="w-full" @click="editField(field)" />
+					</div>
+				</template>
+			</Card>
+		</div>
+
+		<Dialog
+			maximizable
+			modal
+			header="View/Edit Field"
+			v-model:visible="editAndViewVisible"
+			class="w-[95%] md:w-[740px] lg:w-[950px]"
+		>
+			<div class="flex flex-col justify-between items-start gap-4">
+				<div class="flex flex-col gap-2 w-full">
+					<label for="fieldname">Field Name</label>
+					<InputText id="fieldname" class="w-full sm:w-[250px]" :placeholder="selectedField?.field_name" />
+				</div>
+
+				<div class="flex flex-col gap-2 w-full">
+					<label for="croptype">Crop Type</label>
+					<Dropdown
+						class="w-full sm:w-[250px]"
+						v-model="selectedCropType"
+						:options="cropOptions"
+						optionLabel="name"
+						:placeholder="capitalizeFirstCharacter(selectedField?.crop_type as string)"
+						showClear
+					/>
+				</div>
+
+				<div class="flex flex-col gap-2 w-full">
+					<div class="w-full h-[400px] rounded overflow-hidden">
+						<GoogleMapsField
+							:selectedField="selectedField"
+							:fields="teamFields"
+							@update:selectedField="updateSelectedField"
+						/>
+					</div>
+				</div>
+			</div>
+		</Dialog>
+
+		<Toast />
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import GoogleMap from '~/components/GoogleMap.vue'
+import { ref, onMounted } from 'vue'
 import { useConfirm } from 'primevue/useconfirm'
+import Card from 'primevue/card'
+import GoogleMapsField from '~/components/GoogleMapsField.vue'
+import InputText from 'primevue/inputtext'
 
-// get userID
+// TODO: Maybe add toasts to notify users that they have updated or deleted a field
+// TODO: Move Add Field to a new page (with a search bar for places API)
+// TODO: Add controls to edit field on map
+// TODO: Implement Edit Field put request functionality
+// TODO: Add drawing controls to GoogleMapsField on edit field
+
+// PERF: Get team_id and load fields
+const isLoading = ref(true)
 const currentUser = useSupabaseUser()
 
-definePageMeta({
-	middleware: 'auth',
+const teamFields = ref([])
+
+onMounted(async () => {
+	try {
+		isLoading.value = true
+		const teamID = await $fetch('/api/getTeamID', {
+			params: { userid: currentUser?.value?.id },
+		})
+
+		teamFields.value = await $fetch('/api/getTeamFields', {
+			params: { team_id: teamID.team_id },
+		})
+	} catch (error) {
+		console.error('Error fetching user fields:', error)
+	} finally {
+		isLoading.value = false
+	}
 })
 
-const googleMapRef = ref() // Reference to the GoogleMap component
-const isDialogVisible = ref(false)
-const isDrawingEnabled = ref(false)
-const fieldName = ref('')
+// PERF: Dialog on Field info
+
+const editAndViewVisible = ref(false)
+const selectedField = ref(null)
+
+const editField = (field: any) => {
+	selectedField.value = field
+	editAndViewVisible.value = true
+}
+
+function updateSelectedField(newField) {
+	selectedField.value = newField
+}
+
+// PERF: Search by Field Name and filter by Crop Type functionality
+const filteredFields = ref([])
+const searchQuery = ref('')
 const selectedCropType = ref(null)
 const cropOptions = ref([
 	{ name: 'Maize', value: 'maize' },
@@ -56,6 +146,22 @@ const cropOptions = ref([
 	{ name: 'Canola', value: 'canola' },
 	{ name: 'Oats', value: 'oats' },
 ])
+
+watchEffect(() => {
+	filteredFields.value = teamFields.value.filter((field) => {
+		const matchesSearchQuery = field.field_name.toLowerCase().includes(searchQuery.value.toLowerCase())
+		const matchesCropType = selectedCropType.value
+			? field.crop_type.toLowerCase() === selectedCropType.value.value.toLowerCase()
+			: true
+		return matchesSearchQuery && matchesCropType
+	})
+})
+
+const googleMapRef = ref() // Reference to the GoogleMap component
+const isDialogVisible = ref(false)
+const isDrawingEnabled = ref(false)
+const fieldName = ref('')
+
 const fieldsData = ref([])
 const confirm = useConfirm()
 const drawnPolygonPaths = ref<google.maps.LatLng[]>([])
@@ -92,12 +198,6 @@ const showConfirmationDialog = () => {
 		},
 	})
 }
-
-const teamID = await $fetch('/api/getTeamID', {
-	params: { userid: currentUser?.value?.id },
-})
-
-console.log(teamID.team_id)
 
 const saveField = async () => {
 	if (drawnPolygonPaths.value.length > 0) {
@@ -148,7 +248,13 @@ const saveField = async () => {
 	}
 }
 
-const userFields = await $fetch('/api/getTeamFields', {
-	params: { team_id: teamID.team_id },
+function capitalizeFirstCharacter(str: string) {
+	if (!str) return '' // Handle empty strings
+	return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+// PERF: Page Meta
+definePageMeta({
+	middleware: 'auth',
 })
 </script>
