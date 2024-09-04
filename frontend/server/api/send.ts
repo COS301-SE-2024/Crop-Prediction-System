55e6b8ae2376004da4ca28352d50470deb98f80d
Xavier893 <xavierrynolds@gmail.com>
2024-09-04 11:05:49 +0200

    Added url link with team_id in query parameter

--- frontend/server/api/send.ts
+++ frontend/server/api/send.ts
@@ -1,17 +1,19 @@
 import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
+const config = useRuntimeConfig()
 
 export default defineEventHandler(async (event) => {
 	const to = getQuery(event).to
 	const team_id = getQuery(event).team_id
+	const appURL = config.public.appBaseUrl
 
 	try {
 		const data = await resend.emails.send({
 			from: 'Terrabyte <terra@terrabyte.software>',
 			to: [`${to}`],
 			subject: 'Team Invite',
-			html: `<h2>You have been invited!</h2><p>You have been invited to join a team on the terrabyte app. Please use the code below when creating your account.</p><strong>Code: ${team_id}</strong>`,
+			html: `<h2>You have been invited!</h2><p>You have been invited to join a team on the terrabyte app. Please use this code below for refernce when you join the team.</p><strong>Code: ${team_id}</strong> <p>Please follow the link below to register your account.</p><a href="${appURL}/join?team_id=${team_id}">${appURL}/join?team_id=${team_id}</a>`,
 		})
 
 		return data
