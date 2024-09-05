cac51237d4c599ffa72f0458de45d9bf473c0f58
Tiaan Pouwels <97939187+Tiaan2@users.noreply.github.com>
2024-09-05 20:15:54 +0200

    Tailwind setup

--- test/nuxt.config.ts
+++ test/nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from "nuxt/config";
 import lara from "@primevue/themes/lara";
+
 export default defineNuxtConfig({
+  css: ["./assets/css/tailwind.css", "primeicons/primeicons.css"],
   modules: ["@primevue/nuxt-module"],
   primevue: {
     options: {
