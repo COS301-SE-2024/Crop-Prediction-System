1e4ae3906c8810c0fca752339365eec9eeb4e6c8
Tiaan Pouwels <97939187+Tiaan2@users.noreply.github.com>
2024-09-05 20:13:52 +0200

    Update Nuxt configuration to use PrimeVue with themes

--- test/nuxt.config.ts
+++ test/nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+import lara from "@primevue/themes/lara";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: lara,
+      },
+    },
+  },
+});
