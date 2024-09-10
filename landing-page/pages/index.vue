5e6e845f5a7c30c2eaf546559c18018c89f68bf1
Tiaan Pouwels <97939187+Tiaan2@users.noreply.github.com>
2024-09-10 14:14:51 +0200

    added light and dark mode theming for images

--- landing-page/pages/index.vue
+++ landing-page/pages/index.vue
@@ -14,15 +14,10 @@ import Card from 'primevue/card'
 					clip-path: ellipse(150% 87% at 93% 13%);
 				"
 			>
-				<div class="mx-6 md:mx-20 mt-0 md:mt-6">
-					<h1 class="text-6xl font-bold text-gray-900 leading-tight">
-						<span class="font-light block">...</span>Placeholders for images
-					</h1>
-					<p class="font-normal text-2xl leading-normal md:mt-4 text-gray-700">...</p>
-					<Button label="Get Started" as="router-link" to="/" rounded class="!text-xl mt-8 !px-4"></Button>
-				</div>
+				<div class="mx-6 md:mx-20 mt-0 md:mt-6"></div>
 				<div class="flex justify-center md:justify-end">
-					<img src="" alt="Hero Image" class="w-9/12 md:w-auto" />
+					<img src="../assets/image-light.png" alt="Logo" class="object-cover dark:hidden block" />
+					<img src="../assets/image-dark.png" alt="Logo" class="object-cover hidden dark:block" />
 				</div>
 			</div>
 			<div id="features" class="py-12 px-4 sm:px-6 lg:px-8">
@@ -45,7 +40,7 @@ import Card from 'primevue/card'
 								>
 									<i class="pi pi-users text-2xl text-yellow-700 dark:text-yellow-200"></i>
 								</div>
-								<h3 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">Easy to Use</h3>
+								<h2 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">Easy to Use</h2>
 								<p class="text-surface-600 dark:text-surface-200">
 									We have ensured that our product is very user-friendly by designing it such that it is easily
 									understandably and easy to navigate. We also provide a help menus for extra information.
@@ -63,7 +58,7 @@ import Card from 'primevue/card'
 								>
 									<i class="pi pi-palette text-2xl text-cyan-700 dark:text-cyan-200"></i>
 								</div>
-								<h3 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">Fresh Design</h3>
+								<h2 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">Fresh Design</h2>
 								<p class="text-surface-600 dark:text-surface-200">
 									Our product has a modern and sleek design that catches the eye. It implements a bunch of
 									interesting and unique features that makes it stand out.
@@ -81,7 +76,7 @@ import Card from 'primevue/card'
 								>
 									<i class="pi pi-file text-2xl text-indigo-700 dark:text-indigo-200"></i>
 								</div>
-								<h3 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">Well Documented</h3>
+								<h2 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">Well Documented</h2>
 								<p class="text-surface-600 dark:text-surface-200">
 									Our product has comprehensive documentation for easy reference and understanding. We
 									implemented an in-app help menu for extra information.
@@ -94,13 +89,13 @@ import Card from 'primevue/card'
 						>
 							<div class="bg-surface-0 dark:bg-surface-900 h-full p-6 rounded-lg">
 								<div
-									class="flex items-center justify-center w-12 h-12 bg-yellow-200 dark:bg-yellow-700 rounded-lg mb-4"
+									class="flex items-center justify-center w-12 h-12 bg-green-200 dark:bg-green-700 rounded-lg mb-4"
 								>
 									<i class="pi pi-cloud text-2xl text-yellow-700 dark:text-yellow-200"></i>
 								</div>
-								<h3 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">
+								<h2 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">
 									IOT Sensor Integration
-								</h3>
+								</h2>
 								<p class="text-surface-600 dark:text-surface-200">
 									Our product provides thew functionality to integrate with IOT sensors for real-time data. We
 									have a wide range of sensors that can be integrated.
@@ -114,13 +109,13 @@ import Card from 'primevue/card'
 						>
 							<div class="bg-surface-0 dark:bg-surface-900 h-full p-6 rounded-lg">
 								<div
-									class="flex items-center justify-center w-12 h-12 bg-cyan-200 dark:bg-cyan-700 rounded-lg mb-4"
+									class="flex items-center justify-center w-12 h-12 bg-purple-200 dark:bg-purple-700 rounded-lg mb-4"
 								>
 									<i class="pi pi-microchip-ai text-2xl text-cyan-700 dark:text-cyan-200"></i>
 								</div>
-								<h3 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">
+								<h2 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">
 									AI Crop Predictions
-								</h3>
+								</h2>
 								<p class="text-surface-600 dark:text-surface-200">
 									Our product makes use of our in-house state of the art AI model to predict crop yields. This
 									model is is trained and can be used to predict various types of crops.
@@ -134,13 +129,13 @@ import Card from 'primevue/card'
 						>
 							<div class="bg-surface-0 dark:bg-surface-900 h-full p-6 rounded-lg">
 								<div
-									class="flex items-center justify-center w-12 h-12 bg-indigo-200 dark:bg-indigo-700 rounded-lg mb-4"
+									class="flex items-center justify-center w-12 h-12 bg-orange-200 dark:bg-orange-700 rounded-lg mb-4"
 								>
 									<i class="pi pi-map-marker text-2xl text-indigo-700 dark:text-indigo-200"></i>
 								</div>
-								<h3 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">
+								<h2 class="text-xl font-semibold text-surface-900 dark:text-surface-0 mb-2">
 									Google Maps Integration
-								</h3>
+								</h2>
 								<p class="text-surface-600 dark:text-surface-200">
 									Our product allows for easy integration with Google Maps for easy navigation and location
 									access. This improves the user experience, but also the predictions our AI model will make.
@@ -152,8 +147,8 @@ import Card from 'primevue/card'
 			</div>
 			<div id="highlights" class="py-6 px-6 lg:px-20 mx-0 my-12 lg:mx-20">
 				<div class="text-center">
-					<div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-4xl">Powerful Everywhere</div>
-					<span class="text-muted-color text-2xl">Amet consectetur adipiscing elit...</span>
+					<div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-5xl">Highlights of our Product</div>
+					<span class="text-muted-color text-2xl"></span>
 				</div>
 				<div class="grid grid-cols-12 gap-4 mt-20 pb-2 md:pb-20">
 					<div
@@ -169,14 +164,12 @@ import Card from 'primevue/card'
 						>
 							<i class="pi pi-fw pi-mobile !text-4xl text-purple-700"></i>
 						</div>
-						<div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">
-							Congue Quisque Egestas
-						</div>
+						<div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">Mobile Friendly</div>
 						<span
-							class="text-surface-700 dark:text-surface-100 text-2xl leading-normal ml-0 md:ml-2"
+							class="text-surface-700 dark:text-surface-100 text-xl leading-normal ml-0 md:ml-2"
 							style="max-width: 650px"
-							>Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Eget aliquet nibh praesent tristique
-							magna sit amet purus gravida. Sit amet mattis vulputate enim nulla aliquet.</span
+							>Our product is mobile friendly to ensure anyone anywhere can use our product. Our product is built
+							for farmers, therefore mobile friendliness is necessary.</span
 						>
 					</div>
 				</div>
