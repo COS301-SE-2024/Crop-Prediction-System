6553819824bf8ca423da2849116fc982393753c7
Tiaan Pouwels <97939187+Tiaan2@users.noreply.github.com>
2024-09-13 01:06:50 +0200

    Added footer to layouts

--- landing-page/layouts/default.vue
+++ landing-page/layouts/default.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Navbar from '~/components/navbar.vue'
+import Footer from '~/components/footer.vue'
 </script>
 
 <template>
 	<div class="h-screen">
 		<Navbar />
 		<slot />
+		<Footer />
 	</div>
 </template>
