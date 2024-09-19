1cb25c78fed612ece639ba7b2a4502d3e0500a3a
SW1F7YY <stephansmit112@gmail.com>
2024-09-19 10:09:43 +0200

    Refactoring

--- backend/sensors/farmerSensor.py
+++ backend/sensors/farmerSensor.py
@@ -1,12 +1,11 @@
 import serial
 import time
 ser = serial.Serial(
-    port='COM12',\
-    baudrate=5800,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-        timeout=0)
+    port='COM12',
+    baudrate=4800,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE, 
+    bytesize=serial.EIGHTBIT)
 try:
     if ser.is_open:
         # Do something with the serial port
@@ -16,13 +15,24 @@ try:
         # count = 0
         # while count < 10:
         print("Writing data")
-        bytesWritten = ser.write(b'\x01\x03\x00\x00\x00\x07\x04\x08')
+        data_to_send = b'\x01\x04\x00\x00\x00\x04\x44\x09' # 01 04 00 00 00 04 44 09
+        bytesWritten = ser.write(data_to_send)
+        print(f"Bytes written: {bytesWritten}")
+        print(f"Data sent (hex): {data_to_send.hex()}")
+        # sleep for 0.1 second 
+        time.sleep(0.1)
         print(bytesWritten)
         print("Data written")
         print("Reading data")
-        line = ser.readline()
-        # converte the bytes of line to string
-        print(line)
+        start_time = time.time()
+        while time.time() - start_time < 5:  # Wait up to 2 seconds for data
+            if ser.in_waiting:
+                line = ser.read(ser.in_waiting)
+                print(f"Received: {line.hex()}")
+                break
+            time.sleep(0.1)
+        else:
+            print("No data received")
         print("Data read")
         # sleep for 1 second
         time.sleep(1)
