85b912e16f7f07b89c3e76f5f01975a6c1f5b19c
SW1F7YY <stephansmit112@gmail.com>
2024-09-27 20:39:26 +0200

    Add unit test for getNewSensorData function

--- backend/test/test_getData.py
+++ backend/test/test_getData.py
@@ -11,3 +11,8 @@ def test_get_single_line_data():
     result = getNewSensorData(sensorID)
     assert result is not None
 
+def test_get_multiple_line_data():
+    sensorID = '2CF7F12025200009'
+    result = getNewSensorData(sensorID, 5)
+    assert result is not None
+
